1489cd26c89be9b8a0ee1f79024a6a21e7a58913
ThanhNVan <van.xuannhatthanh@gmail.com>
2025-01-02 20:26:47 +0700

    displayed records

--- mainproject/UserSite/views.py
+++ mainproject/UserSite/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import User
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World!. This is the UserSite index page.")
+    users = User.objects.all()
+    context = {"users": users}
+    return render(request, 'user_site/index.html', context)
